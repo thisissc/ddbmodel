3b263d4e27727f6ba605a2dc7e06d2a7b0271e3b
zhoubiao <thisissc@gmail.com>
2024-11-15 16:48:51 +0800

    Tidy offset of every query

--- lastevaluatedkey.go
+++ lastevaluatedkey.go
@@ -12,7 +12,9 @@ func EncodeLastEvaluatedKey(data map[string]*dynamodb.AttributeValue) string {
 		return ""
 	}
 
-	esKeyByte, _ := json.Marshal(data)
+	newData := lastKeyMap(data)
+	esKeyByte, _ := json.Marshal(newData)
+
 	encoded := base64.StdEncoding.EncodeToString(esKeyByte)
 	return encoded
 }
@@ -28,4 +30,56 @@ func DecodeLastEvaluatedKey(data string) map[string]*dynamodb.AttributeValue {
 	}
 
 	return nil
+}
+
+type lastKey struct {
+	B    []byte              `json:"B,omitempty"`
+	BOOL *bool               `json:"BOOL,omitempty"`
+	BS   [][]byte            `json:"BS,omitempty"`
+	L    []*lastKey          `json:"L,omitempty"`
+	M    map[string]*lastKey `json:"M,omitempty"`
+	N    *string             `json:"N,omitempty"`
+	NS   []*string           `json:"NS,omitempty"`
+	NULL *bool               `json:"NULL,omitempty"`
+	S    *string             `json:"S,omitempty"`
+	SS   []*string           `json:"SS,omitempty"`
+}
+
+func newLastKey(attrVal *dynamodb.AttributeValue) *lastKey {
+	newVal := lastKey{
+		B:    attrVal.B,
+		BOOL: attrVal.BOOL,
+		BS:   attrVal.BS,
+		N:    attrVal.N,
+		NS:   attrVal.NS,
+		NULL: attrVal.NULL,
+		S:    attrVal.S,
+		SS:   attrVal.SS,
+	}
+
+	if attrVal.L != nil {
+		valList := make([]*lastKey, len(attrVal.L))
+		for i, val := range attrVal.L {
+			valList[i] = newLastKey(val)
+		}
+		newVal.L = valList
+	}
+
+	if attrVal.M != nil {
+		valMap := make(map[string]*lastKey)
+		for key, val := range attrVal.M {
+			valMap[key] = newLastKey(val)
+		}
+		newVal.M = valMap
+	}
+
+	return &newVal
+}
+
+func lastKeyMap(data map[string]*dynamodb.AttributeValue) map[string]*lastKey {
+	newData := make(map[string]*lastKey)
+	for key, val := range data {
+		newData[key] = newLastKey(val)
+	}
+	return newData
 }
